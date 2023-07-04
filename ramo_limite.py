f7d47cc1b48041f6ef23d3f34ddda8010a47428b
Luciano Pedesol <60025358+lucianopedesol@users.noreply.github.com>
2023-07-04 16:13:17 -0300

    Update ramo_limite.py

--- ramo_limite.py
+++ ramo_limite.py
@@ -1,29 +1,4 @@
-'''
 
-O algoritmo do Ramo e Limite (Branch and Bound) é uma abordagem sistemática que explora recursivamente o espaço de 
-solução do problema, dividindo-o em subproblemas menores e aplicando limites inferiores para eliminar soluções inviáveis.
-
-
-O algoritmo genético é uma abordagem heurística baseada em evolução que tenta encontrar soluções 
-ótimas através de operadores genéticos, como seleção, cruzamento e mutação. Ele é adequado 
-para problemas de otimização combinatória, como o problema da mochila, onde é necessário 
-encontrar uma solução de alta qualidade, mas não necessariamente a solução ótima.
-
-O algoritmo do Ramo e Limite é uma abordagem exata que busca explorar todo o espaço de 
-solução de forma sistemática. Ele divide o problema em subproblemas menores e utiliza limites 
-inferiores para eliminar soluções inviáveis ou subótimas. Essa abordagem garante a obtenção 
-da solução ótima, mas pode ser computacionalmente intensiva, especialmente para problemas grandes.
-
-Portanto, a escolha entre os algoritmos depende das características específicas do problema e das 
-necessidades do contexto. O algoritmo genético pode ser mais rápido e eficiente em problemas de grande 
-escala, enquanto o algoritmo do Ramo e Limite é mais adequado quando se deseja garantir a solução ótima.
-
-É importante ressaltar que cada algoritmo possui vantagens e limitações. O algoritmo genético é 
-uma heurística que depende de parâmetros de configuração e pode não encontrar a solução ótima 
-em todos os casos. Já o algoritmo do Ramo e Limite é exato, mas pode se tornar impraticável 
-para problemas muito grandes devido à complexidade computacional.
-
-'''
 
 class Item:
     def __init__(self, valor, peso):
